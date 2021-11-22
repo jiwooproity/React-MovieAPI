e58e727812c2abadade1ca58415bb1a91bb82cbf
jiwooproity <jiwooproity@naver.com>
2021-11-22 22:15:33 +0900

    영화 웹사이트 react-router-dom 연습

---
+++ React_Movies/src/components/Movie.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+
+function Movie({medium_cover_image, title, summary, genres}) {
+    return (
+        <div>
+            <img src={medium_cover_image} alt={title}/>
+            <h1>
+                <Link to="/movie">{title}</Link>
+            </h1>
+            <p>{summary}</p>
+            <ul>
+                {genres.map(g => (
+                    <li key={g}>{g}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+Movie.propTypes = {
+    medium_cover_image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    geners: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
