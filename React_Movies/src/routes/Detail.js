566159013018b416172a9b180fac7c4aa2b36326
jiwooproity <jiwooproity@naver.com>
2021-11-23 23:19:28 +0900

    React useParam을 이용한 movie.json 정보 불러오기

--- React_Movies/src/routes/Detail.js
+++ React_Movies/src/routes/Detail.js
@@ -1,24 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
+    const [dLoading, setDloading] = useState(true);
+    const [movieInfo, setMovieInfo] = useState([]);
+
     const {id} = useParams();
     
     const getMovie = async() => {
-        const json = await(
-            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        const json = await (
+            await fetch(
+                `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+                )
             ).json();
-            console.log(json);
+            
+        console.log(json.data.movie);
+        setMovieInfo(json.data.movie);
+        setDloading(false);
     };
 
     useEffect(() => {
-    getMovie();
-
+        getMovie();
     }, [])
 
     return (
-        <h1>Detail</h1>
-    )
+        <div>
+            {dLoading ? (
+                <h1>Setting Movie...</h1>
+            ) : (
+                <div>
+                    <img src={movieInfo.medium_cover_image} alt=""/>
+                    <h1>{movieInfo.title}</h1>
+                    <p>{movieInfo.description_full}</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Detail;
--- React_Movies/src/routes/Home.js
+++ React_Movies/src/routes/Home.js
@@ -9,8 +9,8 @@ function Home() {
         const json = await (
             await fetch(
                 `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-            )
-        ).json();
+                )
+            ).json();
 
         setMovies(json.data.movies);
         setLoading(false);
