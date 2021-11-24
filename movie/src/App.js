62b9dc5f7f392fdb892a138cd282075177421ad9
jiwooproity <jiwooproity@naver.com>
2021-11-24 20:16:48 +0900

    영화 웹사이트 자체제작

---
+++ movie/src/App.js
@@ -0,0 +1,12 @@
+import {useState, useEffect} from "react";
+import Navbar from "./Navbar"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ movie/src/Navbar.js
@@ -0,0 +1,23 @@
+import "./css/Navbar.css"
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar__width">
+                <div className="navbar__title">
+                    <h1 className="navbar__logo">TICKET</h1>
+                </div>
+                <div className="navbar__menu">
+                    <ul className="navbar__list">
+                        <li className="navbar__list-item">Movie</li>
+                        <li className="navbar__list-item">Search</li>
+                        <li className="navbar__list-item">How</li>
+                        <li className="navbar__list-item">Help</li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
